efb524f32bd8acd1f722372c7579167f16e39190
Pavel Vinogradov <mr.friderick@ya.ru>
2024-01-11 16:56:48 +0300

    Внесены правки после проверки экспертом

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,31 @@
 fun main() {
-    println("Ваша комиссия составляет: ${calculateCommission(cardType = "Мир", 75001, 5000)}")
+    /*
+    Был уверен, что лимиты в задачу не входят, т.к. речь только о комиссии.
+    Тогда, если я правильно понимаю суть задачи, то:
+    при превышении лимита, мы не считаем комиссию и соответственно программа завершает работу
+    */
+    val monthlyAmount = 1
+    val amount        = 15001
+    val cardType      = "VK Pay"
+
+    when (limitExceeded(cardType, monthlyAmount, amount)) {
+        true -> println("Превышен лимит переводов")
+        else -> println("Ваша комиссия составляет: ${calculateCommission(cardType, monthlyAmount, amount)}")
+    }
+}
+
+fun limitExceeded(cardType: String = "VK Pay", monthlyAmount: Int = 0, amount: Int): Boolean {
+    val limitDayV   = 15000
+    val limitMonthV = 40000
+    val limitDayA   = 150000
+    val limitMonthA = 600000
+
+    return when {
+        cardType == "VK Pay" && ((monthlyAmount + amount > limitMonthV) || amount > limitDayV) -> true
+        // можно было и в первое выражение вставить (под первое true) через ИЛИ, но для читаемости решил вынести отдельно
+        (monthlyAmount + amount > limitMonthA) || amount > limitDayA -> true
+        else -> false
+    }
 }
 
 fun calculateCommission(cardType: String = "VK Pay", monthlyAmount: Int = 0, amount: Int): Int {
@@ -10,7 +36,7 @@ fun calculateCommission(cardType: String = "VK Pay", monthlyAmount: Int = 0, amo
     val minCommissionV        = 35
 
     return when {
-        (cardType == "Mastercard" || cardType == "Maestro") && monthlyAmount > limitM -> (amount * commissionPercentageM).toInt() + commissionAddM
+        (cardType == "Mastercard" || cardType == "Maestro") && monthlyAmount > limitM + amount -> (amount * commissionPercentageM).toInt() + commissionAddM
         (cardType == "Visa" || cardType == "Мир") -> if ((amount * commissionPercentageV) < minCommissionV) {
             minCommissionV
         } else {
